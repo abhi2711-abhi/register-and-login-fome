18e76282f91241be290ca1ca048011e51b80f3ea
abhi2711-abhi <abhi271199@gmail.com>
2021-05-15 12:59:41 +0530

    my project

---
+++ src/models/registers.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const employeeSchema = new mongoose.Schema({
+    firstname : {
+        type:String,
+        required:true
+    },
+    lastname: {
+        type:String,
+        required:true
+    },
+    email: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    gender: {
+        type:String,
+        required:true
+    },  
+    phone: {
+        type:Number,
+        required:true,
+        unique:true
+    },
+    age: {
+        type:Number,
+        required:true
+    },
+    password: {
+        type:String,
+        required:true
+    },
+    confirmpassword: {
+        type:String,
+        required:true
+    },
+    tokens:[{
+        token:{
+            type:String,
+            required:true
+        }
+    }]
+    
+    
+})
+
+
+// generating tokens 
+employeeSchema.methods.generateAuthToken = async function(){
+    try {
+        console.log(this._id);
+        const token = jwt.sign({_id:this._id.toString()}, "mynameabhishekchauhanitsmyfirstproject", {expiresIn:"1 minutes"});
+        this.tokens = this.tokens.concat({token:token});
+        await this.save();
+        return token;
+    } catch (error) {
+        res.send("the error part" + error);
+        console.log("the error part" + error);
+    }
+ }
+
+
+
+// now we need to create a collections
+
+const Register = new mongoose.model("Register", employeeSchema);
+
+module.exports = Register;
